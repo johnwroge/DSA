e46dd3385f31e2293f3da8173b8383a97b7c7b84
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-10-11 21:41:27 -0400

    cleaning up files

--- Python/Dynamic_Programming/house_robber.py
+++ Python/Dynamic_Programming/house_robber.py
@@ -1,46 +1,46 @@
-# class Solution:
-#     # recursive top down without memo
-#     def rob(self, nums: List[int]) -> int:
-#         def helper(nums, i):
-#             if i < 0:
-#                 return 0       
-#             return max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
-#         return helper(nums, len(nums) - 1)
+class Solution:
+    # recursive top down without memo
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums, i):
+            if i < 0:
+                return 0       
+            return max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
+        return helper(nums, len(nums) - 1)
 
     # recursive top down with memo
-    # def rob(self, nums: List[int]) -> int:
-    #     memo = [-1] * len(nums)
-    #     def helper(nums, i):
-    #         if i < 0:
-    #             return 0     
-    #         if memo[i] >= 0:
-    #             return memo[i]  
-    #         result = max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
-    #         memo[i] = result
-    #         return result
-    #     return helper(nums, len(nums) - 1)
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        def helper(nums, i):
+            if i < 0:
+                return 0     
+            if memo[i] >= 0:
+                return memo[i]  
+            result = max(helper(nums, i - 2) + nums[i], helper(nums, i - 1))
+            memo[i] = result
+            return result
+        return helper(nums, len(nums) - 1)
 
     # iterative memo bottom up
-    # def rob(self, nums: List[int]) -> int:
-    #     if len(nums) == 0:
-    #         return 0
-    #     memo = [0] * (len(nums) + 1)
-    #     memo[0] = 0
-    #     memo[1] = nums[0]
-    #     for i in range(1,len(nums)):
-    #         val = nums[i]
-    #         memo[i + 1] = max(memo[i], memo[i - 1] + val)
-    #     return memo[len(nums)]
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        memo = [0] * (len(nums) + 1)
+        memo[0] = 0
+        memo[1] = nums[0]
+        for i in range(1,len(nums)):
+            val = nums[i]
+            memo[i + 1] = max(memo[i], memo[i - 1] + val)
+        return memo[len(nums)]
 
-    # # iterative plus two variables
-    # def rob(self, nums: List[int]) -> int:
-    #     prev1 = 0
-    #     prev2 = 0
-    #     for num in nums:
-    #         temp = max(num + prev1, prev2)
-    #         prev1 = prev2
-    #         prev2 = temp
-    #     return prev2
+    # iterative plus two variables
+    def rob(self, nums: List[int]) -> int:
+        prev1 = 0
+        prev2 = 0
+        for num in nums:
+            temp = max(num + prev1, prev2)
+            prev1 = prev2
+            prev2 = temp
+        return prev2
 
 
    
@@ -58,36 +58,36 @@
 
 
 
-def find_max_sum(nums):
-    n = len(nums)
-    if n == 0:
-        return 0
-    if n == 1:
-        return nums[0]
+# def find_max_sum(nums):
+#     n = len(nums)
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return nums[0]
 
-    dp = [0] * n
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
+#     dp = [0] * n
+#     dp[0] = nums[0]
+#     dp[1] = max(nums[0], nums[1])
 
-    for i in range(2, n):
-        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+#     for i in range(2, n):
+#         dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
-    return dp[n - 1]
+#     return dp[n - 1]
 
-print(find_max_sum([5, 1, 1, 5]))
+# print(find_max_sum([5, 1, 1, 5]))
 
-def find_max_sum(nums):
-    n = len(nums)
-    if n == 0:
-        return 0
-    if n == 1:
-        return nums[0]
+# def find_max_sum(nums):
+#     n = len(nums)
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return nums[0]
 
-    incl = nums[0]
-    excl = 0
-    for i in range(1, n):
-        new_excl = max(incl, excl)
-        incl = excl + nums[i]
-        excl = new_excl
+#     incl = nums[0]
+#     excl = 0
+#     for i in range(1, n):
+#         new_excl = max(incl, excl)
+#         incl = excl + nums[i]
+#         excl = new_excl
 
-    return max(incl, excl)
+#     return max(incl, excl)
--- Python/Graph/Number_of_Islands.py
+++ Python/Graph/Number_of_Islands.py
@@ -1,32 +1,32 @@
 from typing import List
 
 
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         count = 0
-#         visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
         
-#         for x in range(len(grid)):
-#             for y in range(len(grid[0])):
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
                 
-#                 if grid[x][y] == "1" and not visited[x][y]:
-#                     self.dfs(grid, visited, x, y)
-#                     count += 1
+                if grid[x][y] == "1" and not visited[x][y]:
+                    self.dfs(grid, visited, x, y)
+                    count += 1
                           
-#         return count
+        return count
     
 
-#     def dfs(self, grid, visited, x, y):
+    def dfs(self, grid, visited, x, y):
         
-#         if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == "0" or visited[x][y]: 
-#             return 0
-#         if grid[x][y] == "1":
-#             visited[x][y] = True
+        if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == "0" or visited[x][y]: 
+            return 0
+        if grid[x][y] == "1":
+            visited[x][y] = True
 
-#             self.dfs(grid, visited, x + 1, y)  
-#             self.dfs(grid, visited, x - 1, y) 
-#             self.dfs(grid, visited, x, y + 1) 
-#             self.dfs(grid, visited, x, y - 1)
+            self.dfs(grid, visited, x + 1, y)  
+            self.dfs(grid, visited, x - 1, y) 
+            self.dfs(grid, visited, x, y + 1) 
+            self.dfs(grid, visited, x, y - 1)
 
 
          
