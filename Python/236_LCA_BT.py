8c656d8f2ffe1169edefa5b2ec08b7cdc779ea48
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-01 15:53:54 -0400

    assessment

---
+++ Python/236_LCA_BT.py
@@ -0,0 +1,56 @@
+'''
+236. Lowest Common Ancestor of a Binary Tree
+Solved
+Medium
+Topics
+Companies
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between
+two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow
+a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+'''
+
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root, p, q):
+            if not root:
+                return None
+            if root == p or root == q:
+                return root
+            left = dfs(root.left, p, q)
+            right = dfs(root.right, p, q)
+
+            if left and right:
+                return root
+            elif left:
+                return left
+            else:
+                return right
+        return dfs(root, p, q)
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or p == root or q == root:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
+
