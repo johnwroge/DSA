6d9687bd46bfba0e36585a8a146fda0162fa8e9d
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-01 08:40:32 -0400

    215 python

--- Python/215_Kth_Largest.py
+++ Python/215_Kth_Largest.py
@@ -51,4 +51,21 @@ class Solution:
             curr = -1 * heapq.heappop(nums)
         return curr
         
+# optimized frequency counting approach
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_value = min(nums)
+        max_value = max(nums)
+        # this is a modification to negative min value
+        count = [0] * (max_value - min_value + 1)
+
+        for num in nums:
+            count[num - min_value] += 1
         
+        remain = k
+        for num in range(len(count) -1, -1, -1):
+            remain -= count[num]
+            if remain <= 0:
+                return num + min_value
+
+        return -1
