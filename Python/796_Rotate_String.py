076eee9588027fa060b833cd1608467356f4eddc
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-02 20:39:38 -0400

    796

---
+++ Python/796_Rotate_String.py
@@ -0,0 +1,38 @@
+'''
+796. Rotate String
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+
+
+'''
+
+'''
+Solution: 
+
+You can use brute force to check every possible candidate, or you can advantage of string concatenation. 
+If you concatenate s with itself its easy to check if goal is located within it. 
+'''
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return any(s[i:] + s[:i] == goal for i in range(len(s)))
+
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and goal in s + s
+
