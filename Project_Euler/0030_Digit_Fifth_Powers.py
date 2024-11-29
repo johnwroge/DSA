73f6c27a8ab54e7710ea61efb7b19a5e27ee3368
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-28 20:54:23 -0500

    28, 29 python

--- Project_Euler/0030_Digit_Fifth_Powers.py
+++ Project_Euler/0030_Digit_Fifth_Powers.py
@@ -11,6 +11,20 @@ The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 '''
 
+
+def find_cutoff(power):
+    max_digit_sum = lambda d: d * (9 ** power)
+    smallest_number = lambda d: 10 ** (d - 1)
+    
+    d = 1
+    while max_digit_sum(d) >= smallest_number(d):
+        d += 1
+
+    return max_digit_sum(d - 1)
+
+cutoff = find_cutoff(5)
+print(f"The cutoff for 5th powers is: {cutoff}")
+
 def is_valid(num):
     total = [int(n) ** 5 for n in list(str(num))]
     total = sum(total)
@@ -19,11 +33,10 @@ def is_valid(num):
 def find_candidates():
     result = []
     for j in range(2, 1000000):
-        can, total = is_valid(j)
-        if can:
-            # print(total)
+        valid, total = is_valid(j)
+        if valid:
             result.append(j)
     return sum(result)
 
 
-# print(find_candidates())
+print(find_candidates())
