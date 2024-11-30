687c42291ca882c7f5fcead1bb62456c937ef3ae
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-11-30 13:01:32 -0500

    formatting

--- Project_Euler/0006_sum_square_diff.py
+++ Project_Euler/0006_sum_square_diff.py
@@ -14,11 +14,11 @@ ten natural numbers and the square of the sum is
 3025 - 385 = 2640
 
 Find the difference between the sum of the squares of the first one
- hundred natural numbers and the square of the sum.
+hundred natural numbers and the square of the sum.
 
- (1^2 + 2^2 + ...100^2) = 
+(1^2 + 2^2 + ...100^2) = 
 
- (1 + 2 + 3 + ... 100)^2
+(1 + 2 + 3 + ... 100)^2
 
 '''
 
