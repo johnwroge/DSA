9a85bff4edbadc93a478888a66d7c7cadaa6879c
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-03 20:57:50 -0500

    2825 py

---
+++ Python/2825_Make_Substring.py
@@ -0,0 +1,59 @@
+'''
+You are given two 0-indexed strings str1 and str2.
+
+In an operation, you select a set of indices in str1, and for each index i in the set, increment str1[i] to the next character cyclically. That is 'a' becomes 'b', 'b' becomes 'c', and so on, and 'z' becomes 'a'.
+
+Return true if it is possible to make str2 a subsequence of str1 by performing the operation at most once, and false otherwise.
+
+Note: A subsequence of a string is a new string that is formed from the original string by deleting some (possibly none) of the characters without disturbing the relative positions of the remaining characters.
+
+ 
+
+Example 1:
+
+Input: str1 = "abc", str2 = "ad"
+Output: true
+Explanation: Select index 2 in str1.
+Increment str1[2] to become 'd'. 
+Hence, str1 becomes "abd" and str2 is now a subsequence. Therefore, true is returned.
+Example 2:
+
+Input: str1 = "zc", str2 = "ad"
+Output: true
+Explanation: Select indices 0 and 1 in str1. 
+Increment str1[0] to become 'a'. 
+Increment str1[1] to become 'd'. 
+Hence, str1 becomes "ad" and str2 is now a subsequence. Therefore, true is returned.
+Example 3:
+
+Input: str1 = "ab", str2 = "d"
+Output: false
+Explanation: In this example, it can be shown that it is impossible to make str2 a subsequence of str1 using the operation at most once. 
+Therefore, false is returned.
+ 
+'''
+
+
+class Solution:
+    def canMakeSubsequence(self, s1: str, s2: str) -> bool:
+        pattern = 'abcdefghijklmnopqrstuvwxyz'
+        letters = list(pattern)
+        change = False
+        i = 0
+        j = 0
+        while i < len(s1) and j < len(s2):
+            if s1[i] != s2[j]:
+                index = letters.index(s1[i]) + 1
+                if index == 26:
+                    index = 0
+                print(index, letters[index], s2[j])
+                if index < len(letters) and letters[index] == s2[j]:
+                    change = True
+                    i += 1
+                    j += 1
+                else:
+                    i += 1
+            else:
+                i += 1
+                j += 1
+        return j == len(s2)
