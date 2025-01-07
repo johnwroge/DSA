781fb458d5f1364d5045b576d34abdf5f95b9442
John Wroge <72668920+johnwroge@users.noreply.github.com>
2025-01-07 08:13:29 -0500

    1408 python

---
+++ Python/1408_String_Matching.py
@@ -0,0 +1,48 @@
+'''
+Given an array of string words, return all strings in words that
+is a substring of another word. You can return the answer in any
+order.
+
+A substring is a contiguous sequence of characters within a string
+
+
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of
+"superhero".
+["hero","as"] is also a valid answer.
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+Explanation: No string of words is substring of another string.
+'''
+from typing import List
+
+# brute force O(m^2 x n^2)
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        result = set()
+        for i in range(len(words)):
+            word = words[i]
+            for w in words:
+                if word != w:
+                    if word in w:
+                        result.add(word)
+        return list(result)
+
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        r = ' '.join(words)
+        return [i for i in words if r.count(i) > 1]
+        
