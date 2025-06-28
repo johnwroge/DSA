82f66654e0a86598c5925ae644b118749d8e8ed3
John Wroge <wrogejohn@gmail.com>
2025-06-28 17:31:44 -0400

    38 truncatable

---
+++ Project_Euler/0037_Truncatable_Primes.py
@@ -0,0 +1,57 @@
+'''
+The number 3797 has an interesting property. Being prime itself, it is possible
+to continuously remove digits from left to right, and remain prime at each stage:
+3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+Find the sum of the only eleven primes that are both truncatable from left to right
+and right to left. NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+'''
+
+
+from math import floor
+
+
+from math import floor
+
+
+def is_prime(n):
+    """Check if a number is prime"""
+    if n < 2:
+        return False
+    if n in {2, 3, 5, 7}:
+        return True
+    if n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+        return False
+    
+    # Check odd divisors starting from 7
+    for i in range(7, floor(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_truncatable(n):
+    to_check = str(n)
+    
+    if not is_prime(n):
+        return False
+    
+    for i in range(1, len(to_check)):
+        right_to_left = int(to_check[:len(to_check) - i])  
+        left_to_right = int(to_check[i:])  
+        
+        if not is_prime(right_to_left) or not is_prime(left_to_right):
+            return False
+    return True
+
+
+def solution():
+    answer = []
+    n = 11  
+    while len(answer) < 11:
+        if is_truncatable(n):
+            answer.append(n)
+        n += 2 if n > 2 else 1  
+    return sum(answer)
+        
+
+print(solution())
