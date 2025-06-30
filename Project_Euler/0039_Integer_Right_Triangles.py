03bf5e7c83b13f4950c6ec16e726f2a492d0ea94
John Wroge <wrogejohn@gmail.com>
2025-06-29 20:18:09 -0400

    40

--- Project_Euler/0039_Integer_Right_Triangles.py
+++ Project_Euler/0039_Integer_Right_Triangles.py
@@ -4,7 +4,7 @@ there are exactly three solutions for p = 120.
 
 {20,48,52}, {24,45,51}, {30,40,50}
 
-For which value of p ≤ 1000, is the number of solutions maximised?
+For which value of p ≤ 1000, is the number of solutions maximized?
 """
 
 def is_a_right_triangle(a, b, c):
